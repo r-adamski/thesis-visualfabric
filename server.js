d12ee3d03fb7d004320d47480af825ee0937f7cc
r-adamski <arafair@gmail.com>
2020-11-10 18:57:59 +0100

    ts init

--- server.js
+++ server.js
@@ -1,95 +1,127 @@
 'use strict';
-
-const { Wallets, Gateway } = require('fabric-network');
-const fs = require('fs');
-const path = require('path');
-
-
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+var _a = require('fabric-network'), Wallets = _a.Wallets, Gateway = _a.Gateway;
+var fs = require('fs');
+var path = require('path');
 // simple map to hold blocks for processingg
-class BlockMap {
-    constructor() {
-        this.list = []
+var BlockMap = /** @class */ (function () {
+    function BlockMap() {
+        this.list = [];
     }
-    get(key) {
+    BlockMap.prototype.get = function (key) {
         key = parseInt(key, 10).toString();
-        return this.list[`block${key}`];
-    }
-    set(key, blockData) {
-
-    	console.log(`------------------------------------------------`);
-        console.log(`Converting block number: ${key}`);
-    	let blockDataJSON = this.convertBlockToJSON(blockData);
-    	console.log(`------------------------------------------------`);
-
-        this.list[`block${key}`] = blockData;
-    }
-    remove(key) {
+        return this.list["block" + key];
+    };
+    BlockMap.prototype.set = function (key, blockData) {
+        console.log("------------------------------------------------");
+        console.log("Converting block number: " + key);
+        var blockDataJSON = this.convertBlockToJSON(blockData);
+        console.log("------------------------------------------------");
+        this.list["block" + key] = blockData;
+    };
+    BlockMap.prototype.remove = function (key) {
         key = parseInt(key, 10).toString();
-        delete this.list[`block${key}`];
-    }
-    convertBlockToJSON(blockData) {
-    	console.log(blockData);
-    }
+        delete this.list["block" + key];
+    };
+    BlockMap.prototype.convertBlockToJSON = function (blockData) {
+        console.log(blockData);
+    };
+    return BlockMap;
+}());
+var ProcessingMap = new BlockMap();
+function main() {
+    return __awaiter(this, void 0, void 0, function () {
+        var walletPath, wallet, userExists, ccpPath, ccp, gateway, network, listener, error_1;
+        var _this = this;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0:
+                    _a.trys.push([0, 6, , 7]);
+                    walletPath = path.join(process.cwd(), 'wallet');
+                    return [4 /*yield*/, Wallets.newFileSystemWallet(walletPath)];
+                case 1:
+                    wallet = _a.sent();
+                    console.log("Wallet path: " + walletPath);
+                    return [4 /*yield*/, wallet.get('appUser')];
+                case 2:
+                    userExists = _a.sent();
+                    if (!userExists) {
+                        console.log('An identity for the user "appUser" does not exist in the wallet');
+                        console.log('Run the enrollUser.js application before retrying');
+                        return [2 /*return*/];
+                    }
+                    ccpPath = path.resolve(__dirname, '..', '..', 'test-network', 'organizations', 'peerOrganizations', 'org1.example.com', 'connection-org1.json');
+                    ccp = JSON.parse(fs.readFileSync(ccpPath, 'utf8'));
+                    gateway = new Gateway();
+                    return [4 /*yield*/, gateway.connect(ccp, { wallet: wallet, identity: 'appUser', discovery: { enabled: true, asLocalhost: true } })];
+                case 3:
+                    _a.sent();
+                    return [4 /*yield*/, gateway.getNetwork('mychannel')];
+                case 4:
+                    network = _a.sent();
+                    return [4 /*yield*/, network.addBlockListener(function (block) { return __awaiter(_this, void 0, void 0, function () {
+                            return __generator(this, function (_a) {
+                                switch (_a.label) {
+                                    case 0: 
+                                    // Add the block to the processing map by block number
+                                    return [4 /*yield*/, ProcessingMap.set(block.blockData.header.number, block.blockData)];
+                                    case 1:
+                                        // Add the block to the processing map by block number
+                                        _a.sent();
+                                        console.log("Added block " + block.blockData.header.number + " to ProcessingMap");
+                                        return [2 /*return*/];
+                                }
+                            });
+                        }); }, 
+                        // set the starting block for the listener
+                        { filtered: false, startBlock: 0 })];
+                case 5:
+                    listener = _a.sent();
+                    console.log("Listening for block events...");
+                    return [3 /*break*/, 7];
+                case 6:
+                    error_1 = _a.sent();
+                    console.error("Failed to evaluate transaction: " + error_1);
+                    process.exit(1);
+                    return [3 /*break*/, 7];
+                case 7: return [2 /*return*/];
+            }
+        });
+    });
 }
-
-
-
-let ProcessingMap = new BlockMap();
-
-async function main() {
-
-
-//start express server and sockets
-
-
-    try {
-        // Create a new file system based wallet for managing identities.
-        const walletPath = path.join(process.cwd(), 'wallet');
-        const wallet = await Wallets.newFileSystemWallet(walletPath);
-        console.log(`Wallet path: ${walletPath}`);
-
-        // Check to see if we've already enrolled the user.
-        const userExists = await wallet.get('appUser');
-        if (!userExists) {
-            console.log('An identity for the user "appUser" does not exist in the wallet');
-            console.log('Run the enrollUser.js application before retrying');
-            return;
-        }
-
-        // Parse the connection profile. This would be the path to the file downloaded
-        // from the IBM Blockchain Platform operational console.
-        const ccpPath = path.resolve(__dirname, '..', '..', 'test-network','organizations','peerOrganizations','org1.example.com', 'connection-org1.json');
-        const ccp = JSON.parse(fs.readFileSync(ccpPath, 'utf8'));
-        // Create a new gateway for connecting to our peer node.
-        const gateway = new Gateway();
-        await gateway.connect(ccp, { wallet, identity: 'appUser', discovery: { enabled: true, asLocalhost: true } });
-
-        // Get the network (channel) our contract is deployed to.
-        const network = await gateway.getNetwork('mychannel');
-
-
-
-        const listener = await network.addBlockListener(
-            async (block) => {
-
-
-                // Add the block to the processing map by block number
-                await ProcessingMap.set(block.blockData.header.number, block.blockData);
-
-                console.log(`Added block ${block.blockData.header.number} to ProcessingMap`);
-
-            },
-            // set the starting block for the listener
-            { filtered: false, startBlock: 0 }
-        );
-
-        console.log(`Listening for block events...`);
-
-
-    } catch (error) {
-        console.error(`Failed to evaluate transaction: ${error}`);
-        process.exit(1);
-    }
-}
-
 main();
---
+++ server.ts
@@ -0,0 +1,95 @@
+'use strict';
+
+const { Wallets, Gateway } = require('fabric-network');
+const fs = require('fs');
+const path = require('path');
+
+
+// simple map to hold blocks for processingg
+class BlockMap {
+    constructor() {
+        this.list = []
+    }
+    get(key) {
+        key = parseInt(key, 10).toString();
+        return this.list[`block${key}`];
+    }
+    set(key, blockData) {
+
+    	console.log(`------------------------------------------------`);
+        console.log(`Converting block number: ${key}`);
+    	let blockDataJSON = this.convertBlockToJSON(blockData);
+    	console.log(`------------------------------------------------`);
+
+        this.list[`block${key}`] = blockData;
+    }
+    remove(key) {
+        key = parseInt(key, 10).toString();
+        delete this.list[`block${key}`];
+    }
+    convertBlockToJSON(blockData) {
+    	console.log(blockData);
+    }
+}
+
+
+
+let ProcessingMap = new BlockMap();
+
+async function main() {
+
+
+//start express server and sockets
+
+
+    try {
+        // Create a new file system based wallet for managing identities.
+        const walletPath = path.join(process.cwd(), 'wallet');
+        const wallet = await Wallets.newFileSystemWallet(walletPath);
+        console.log(`Wallet path: ${walletPath}`);
+
+        // Check to see if we've already enrolled the user.
+        const userExists = await wallet.get('appUser');
+        if (!userExists) {
+            console.log('An identity for the user "appUser" does not exist in the wallet');
+            console.log('Run the enrollUser.js application before retrying');
+            return;
+        }
+
+        // Parse the connection profile. This would be the path to the file downloaded
+        // from the IBM Blockchain Platform operational console.
+        const ccpPath = path.resolve(__dirname, '..', '..', 'test-network','organizations','peerOrganizations','org1.example.com', 'connection-org1.json');
+        const ccp = JSON.parse(fs.readFileSync(ccpPath, 'utf8'));
+        // Create a new gateway for connecting to our peer node.
+        const gateway = new Gateway();
+        await gateway.connect(ccp, { wallet, identity: 'appUser', discovery: { enabled: true, asLocalhost: true } });
+
+        // Get the network (channel) our contract is deployed to.
+        const network = await gateway.getNetwork('mychannel');
+
+
+
+        const listener = await network.addBlockListener(
+            async (block) => {
+
+
+                // Add the block to the processing map by block number
+                await ProcessingMap.set(block.blockData.header.number, block.blockData);
+
+                console.log(`Added block ${block.blockData.header.number} to ProcessingMap`);
+
+            },
+            // set the starting block for the listener
+            { filtered: false, startBlock: 0 }
+        );
+
+        console.log(`Listening for block events...`);
+
+
+    } catch (error) {
+        console.error(`Failed to evaluate transaction: ${error}`);
+        process.exit(1);
+    }
+}
+
+main();
