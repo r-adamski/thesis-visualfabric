027b1725d2c500b197363b082e86ff79088e2e54
r-adamski <arafair@gmail.com>
2020-12-12 15:47:45 +0100

    sdf

--- server.js
+++ server.js
@@ -110,7 +110,7 @@ function parseFabricBlock(block) {
                     var single_write = {
                         key: write.key,
                         is_delete: write.is_delete,
-                        value: write.value.toString('hex')
+                        value: write.value.toString()
                     };
                     parsed_writes.push(single_write);
                 });
