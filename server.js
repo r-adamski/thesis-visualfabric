c21edd28785061d0375b345e9138ef2fe80e6b14
r-adamski <arafair@gmail.com>
2020-12-12 16:14:02 +0100

    asd

--- server.js
+++ server.js
@@ -110,7 +110,7 @@ function parseFabricBlock(block) {
                     var single_write = {
                         key: write.key,
                         is_delete: write.is_delete,
-                        value: write.value.toString()
+                        value: write.value.toString('utf-8')
                     };
                     parsed_writes.push(single_write);
                 });
